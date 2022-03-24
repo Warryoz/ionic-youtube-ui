dee039d429f41ac240c3a623a809f1224496713d
Geronimo Velasco <masafesio.10@gmail.com>
2022-03-23 21:16:09 -0500

    first commit

---
+++ src/app/directives/hideheader.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HideheaderDirective } from './hideheader.directive';
+
+describe('HideheaderDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HideheaderDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/shared-directives.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HideheaderDirective } from './hideheader.directive';
+
+
+
+@NgModule({
+  declarations: [
+    HideheaderDirective
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedDirectivesModule { }
